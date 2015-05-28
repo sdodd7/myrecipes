012037b343405b0793372cdde76d231d3454816f
sdodd77 <dodd7@hotmail.com>
2015-05-28 13:14:22 +0000

    Create chefs and recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150528130609) do
+
+  create_table "chefs", force: true do |t|
+    t.string "chefname"
+    t.string "email"
+  end
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
