038b6ae9b4eb362a90fa41891adbb60e6af4ad11
sdodd77 <dodd7@hotmail.com>
2015-06-27 16:45:23 +0000

    Added form submission, new create, built show recipes page error messages

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,44 @@
+class RecipesController < ApplicationController
+  
+  def index
+    @recipes = Recipe.all
+  end
+  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(1)
+    
+    if @recipe.save
+      flash[:success] = "Your recipe was created successfully!"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+  
+  def destroy
+    
+  end
+  
+end
