e9486fc4a19d1acb71133279a6dc48b364d7b031
sdodd77 <dodd7@hotmail.com>
2015-06-28 08:43:22 +0000

    Create new and edit forms and partials

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,11 +13,17 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    
+    @recipe = Recipe.find(params[:id])
   end
   
   def update
-    
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
   end
   
   def create
