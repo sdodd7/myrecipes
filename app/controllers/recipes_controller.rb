cb99259a1c19b427c18b0aeb54a3b6de87e88bce
sdodd77 <dodd7@hotmail.com>
2015-07-25 14:26:55 +0000

    Added styles and ingredients views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
   
   before_action :set_recipe, only: [:edit, :update, :show, :like]
-  before_action :require_user, except: [:show, :index]
+  before_action :require_user, except: [:show, :index, :like]
+  before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
   
   def index
@@ -70,6 +71,16 @@ class RecipesController < ApplicationController
   
   def destroy
     
+  end
+  
+
+
+
+ def require_user_like
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform this action!"
+      redirect_to :back
+    end
   end
   
 end
